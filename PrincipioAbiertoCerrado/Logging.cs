e8872feada8d98f7684bcae29075168e3cfd51f0
Marcos Rivero <marcosrivero_d4@hotmail.com>
2023-08-31 15:14:53 -0300

    Primer Commit

---
+++ PrincipioAbiertoCerrado/Logging.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+
+namespace PrincipioAbiertoCerrado
+{
+    public class Logging
+    {
+        private readonly LoggingRepositorio _loggingRepositorio;
+        public Logging() { 
+            _loggingRepositorio = new LoggingRepositorio();
+        }
+        public void Informacion(string mensaje)
+        {
+            Log.Information(mensaje);
+            _loggingRepositorio.Guardar();
+        }
+
+        public void Error(string mensaje, Exception e)
+        {
+            Log.Error(mensaje, e);
+            _loggingRepositorio.Guardar();
+        }
+
+        public virtual void Fatal(string mensaje, Exception e)
+        {
+            Log.Information(mensaje, e);
+            _loggingRepositorio.Guardar();
+        }
+    }
+}
---
+++ PrincipioAbiertoCerrado/LoggingDB.cs
@@ -0,0 +1,16 @@
+﻿namespace PrincipioAbiertoCerrado
+{
+    public class LoggingDB : Logging
+    {
+        private readonly LoggingRepositorio _logginRepositorio;
+        public LoggingDB() { 
+            _logginRepositorio = new LoggingRepositorio();
+        }
+
+        public override void Fatal(string mensaje, Exception e)
+        {
+            base.Fatal(mensaje, e);
+            _logginRepositorio.Guardar();
+        }
+    }
+}
